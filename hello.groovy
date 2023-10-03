42f8f9a465e158e4d265956cbc21148d76adc57d
minhvuong0499 <minhvuong0499@gmail.com>
2023-10-03 14:57:49 +0700

    add class file groovy from branch2

--- hello.groovy
+++ hello.groovy
@@ -4,3 +4,11 @@ def x = 104
 println x.getClass()
 x = "Guru99"
 println x.getClass()
+
+public class Demo {
+    public static void main(String[] args) {
+        for (int x = 0; x <= 5; x++) {
+            System.out.println(x);
+        }
+    }
+}
